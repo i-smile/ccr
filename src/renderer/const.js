8d957125e9c597d078c3e0ebf30a25fe1bfec3e8
wangyuan <875441795@qq.com>
2019-03-03 13:30:55 +0800

    打印页改成保存所有图片

--- src/renderer/const.js
+++ src/renderer/const.js
@@ -4,4 +4,6 @@ const app = electron.app || electron.remote.app
 
 export const userDir = app.getPath('userData')
 
+export const desktopDir = app.getPath('desktop')
+
 export const version = '1.1.0'
--- src/renderer/views/PrintPage.vue
+++ src/renderer/views/PrintPage.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="print-page">
     <div class="dot-left" @click="navBack"></div>
+    <!-- <div class="dot-right" @click="saveAll"></div> -->
     <div class="title">
       <h4>Ceramics Fitting Room</h4>
       <p>
@@ -11,10 +12,12 @@
     <ul class="list">
       <li v-for="index in [0, 1, 2]" :key="index">
         <canvas :id="'canvas' + index" :width="canvasWidth" :height="canvasHeight"></canvas>
-        <div class="dot dot-print" @click="print(index)"></div>
-        <div class="dot dot-save" @click="save(index)"></div>
+        <!-- <div class="dot dot-print" @click="print(index)"></div> -->
+        <!-- <div class="dot dot-save" @click="save(index)"></div> -->
       </li>
     </ul>
+
+    <div class="dot-all" @click="saveAll"></div>
 
     <div class="logo"  @click="navToStartPage">
       <img src="../assets/logo.png">
@@ -24,12 +27,13 @@
 
 <script>
 import fs from 'fs'
+import fse from 'fs-extra'
 import path from 'path'
 // import { mapState } from 'vuex';
 import { ipcRenderer } from 'electron'
 import { mapState, mapActions } from 'vuex'
 import Render from '@/components/Render'
-import { userDir } from '@/const'
+import { userDir, desktopDir } from '@/const'
 import bgImgUrl from '@/assets/print-bg.jpg'
 import { saveAsJPEG } from 'canvas2image-es6'
 
@@ -49,9 +53,9 @@ export default {
   },
   mounted () {
     const that = this
-    this.canvasList = document.getElementsByTagName('canvas');
+    this.canvasList = document.getElementsByTagName('canvas')
 
-    [0, 1, 2].forEach(index => {
+    ;[0, 1, 2].forEach(index => {
       const canvas = that.canvasList[index]
       const ctx = canvas.getContext('2d')
 
@@ -95,10 +99,38 @@ export default {
     },
     save (index) {
       saveAsJPEG(this.canvasList[index], undefined, undefined, `picture${index + 1}.jpg`)
+    },
+    saveAll () {
+      fse.ensureDirSync(`${desktopDir}/cfr-pics/`)
+      ;[0, 1, 2].forEach(index => {
+        let imgData = this.canvasList[index].toDataURL('image/png')
+        const base64Data = imgData.replace(/^data:image\/\w+;base64,/, '')
+        const dataBuffer = Buffer.from(base64Data, 'base64')
+        fs.writeFile(`${desktopDir}/cfr-pics/picture${index + 1}.png`, dataBuffer, function (err) {
+          if (err) {
+            console.log(err)
+            return
+          }
+          console.log('图片保存成功')
+          if (index === 2) {
+            M.toast({html: '图片保存成功!', displayLength: 600})
+          }
+        })
+      })
     }
   }
 }
 </script>
+
+<style>
+#toast-container {
+  top: 60% !important;
+  left: 50% !important;
+  width: 160px;
+  text-align: center;
+  transform: translate(-50%, 0);
+}
+</style>
 
 <style lang="scss" scoped>
 .print-page {
@@ -116,6 +148,30 @@ export default {
   border-radius: 24px;
   cursor: pointer;
   background-color: #eacfc8;
+  z-index: 9;
+}
+.dot-right {
+  position: absolute;
+  top: 30px;
+  right: 30px;
+  width: 24px;
+  height: 24px;
+  border-radius: 24px;
+  cursor: pointer;
+  background-color: #8e9ec0;
+  z-index: 9;
+}
+
+.dot-all {
+  position: absolute;
+  bottom: 22%;
+  left: 50%;
+  width: 30px;
+  height: 30px;
+  border-radius: 30px;
+  cursor: pointer;
+  transform: translate(-50%, 0);
+  background-color: #8e9ec0;
   z-index: 9;
 }
 
